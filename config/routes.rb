f092cfd9df1e08b353226742837a185c47430b26
Yosa Yon <yosa.yon@gmail.com>
2018-05-07 23:39:46 +0000

    added statics controller for homepage, added root route and navigation bar to homepage with css style. Added partial for navigation for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :orders
-  resources :carts
-  resources :line_items
-  resources :products
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :reviews
+  #resources :orders
+  #resources :carts
+  #resources :line_items
+  #resources :products
+  #resources :users
+   root 'statics#home'
 end
