f71149023051784e203b4e1452f1a7435505093b
Yosa Yon <yosa.yon@gmail.com>
2018-05-09 22:58:44 +0000

    added a helper method called show_name_if_signed_in to remove the signnin link from the navigation bar and replace it with the users name with logout as an option instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
  
- def signed_in
+ def show_name_if_signed_in
+  if logged_in?
+   render :partial => 'layouts/nav_name'
+  else
+   render :partial => 'layouts/nav_sign_in'
+  end
  end
+ 
 end
