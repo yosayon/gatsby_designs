3a687876a41c66c9fe8272649b0b542019635532
Yosa Yon <yosa.yon@gmail.com>
2018-05-09 21:48:47 +0000

    added new, show, user_params, and find_user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+ before_action :find_user!, :only => [:create]
  
  def new
   @user = User.new
+ end
+ 
+ def show
+ end
+ 
+ private
+ 
+ def user_params
+  params.require(:user).permit(:name, :email, :password)
+ end
+ 
+ def find_user!
+  @user = User.find(params[:id])
  end
 end
