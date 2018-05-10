ac5d48881b413b60d99ded33404aa562e96d530b
Yosa Yon <yosa.yon@gmail.com>
2018-05-10 02:55:44 +0000

    added users#create for when a user signups via webpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
- before_action :find_user!, :only => [:create]
  
  def new
   @user = User.new
+ end
+ 
+ def create
+  user = User.new(user_params)
+  if user.save
+   session[:user_id] = user.id
+   redirect_to root_path
+  else
+   render :new
+  end
  end
  
  def show
