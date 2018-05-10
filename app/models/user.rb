439d952826efdc4a529e2515ba2a93bf42aa3938
Yosa Yon <yosa.yon@gmail.com>
2018-05-10 05:41:44 +0000

    updated navigation options

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
  has_secure_password
  has_many :orders
  has_many :reviews
+ validates :name, :presence => true
+ validates :email, :presence => true, :uniqueness => true
+ validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+ validates :password, :confirmation => true
+ validates :password, length: 6..20
+ 
  
  def self.find_or_create_by_omniauth(auth)
   where(:email => auth.info.email).first_or_create do |user|
