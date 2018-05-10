2281e7ee0e1c968a99494b682d4fc8aebb74847b
Yosa Yon <yosa.yon@gmail.com>
2018-05-10 02:57:39 +0000

    added find_or_create_by_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+ has_secure_password
  has_many :orders
  has_many :reviews
- has_secure_password
+ 
+ def self.find_or_create_by_omniauth(auth)
+  where(:email => auth.info.email).first_or_create do |user|
+   user.id = auth.info.uid
+   user.name = auth.info.name
+   user.password = SecureRandom.hex
+   end
+  end
 end
