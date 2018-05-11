842afa6371f5d02e35bf5a26b1126ce57165c7f4
Yosa Yon <yosa.yon@gmail.com>
2018-05-11 05:41:30 +0000

    edited class method to assign user is to fb uid and altered secure random hex to generate 20 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
  
  def self.find_or_create_by_omniauth(auth)
   where(:email => auth.info.email).first_or_create do |user|
-   user.id = auth.info.uid
+   user.id = auth.uid
    user.name = auth.info.name
-   user.password = SecureRandom.hex
+   user.password = SecureRandom.hex(10)
    end
   end
 end
