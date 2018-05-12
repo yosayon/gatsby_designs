1491646929220fd8adc3c59559389ec9607beb1c
Yosa Yon <yosa.yon@gmail.com>
2018-05-12 03:40:14 +0000

    added products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = [
+ {:name => "Broken Joe", :price => 184},
+ {:name => "Dave", :price => 120},
+ {:name => "Andrea", :price => 120},
+ {:name => "Scotty", :price => 155},
+ {:name => "Common", :price => 290},
+ {:name => "Be Frank", :price => 310},
+ {:name => "Asinine", :price => 230},
+ {:name => "Corrupt", :price => 135},
+ {:name => "Bleak", :price => 330},
+ {:name => "Lust", :price => 285},
+ {:name => "Hand Me Downs", :price => 990},
+ {:name => "Obvious", :price => 138},
+ {:name => "Mental", :price => 225},
+ {:name => "Question", :price => 312},
+ {:name => "Karma", :price => 549}
+ ]
+ 
+ products.each do |product|
+  Product.create(product)
+ end
