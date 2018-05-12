d5b5cb4edf336e8fdbf9a7c45e879ea546f931e2
Yosa Yon <yosa.yon@gmail.com>
2018-05-12 19:21:16 +0000

    formatting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
- 
+ before_action :find_product!, :only => [:show]
  def index
   @products = Product.all
+ end
+ 
+ def show
+ end
+ 
+ private
+ 
+ def find_product!
+  @product = Product.find(params[:id])
  end
  
 end
