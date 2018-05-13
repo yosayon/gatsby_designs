7e6591edd2bfe46f2a852023d1f1a4820ec18387
Yosa Yon <yosa.yon@gmail.com>
2018-05-13 04:41:16 +0000

    added update action and cart_params private method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,19 @@
 class CartsController < ApplicationController
+ 
+ def show
+ end
+ 
+ def update
+  @user = User.find(params[:user_id])
+  #checking out just means "paying" and starting with a new cart
+  @user.check_out_order
+  @user.current_order.check_out_order
+  redirect_to order_path
+ end
+ 
+ private
+ 
+ def cart_params
+  params.require(:cart).permit(:user_id)
+ end
 end
