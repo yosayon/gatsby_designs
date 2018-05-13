eeaf2aa860966cc57f4c74fac09f4f4449acbc0f
Yosa Yon <yosa.yon@gmail.com>
2018-05-13 04:43:18 +0000

    added create action and line_item_params method

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,2 +1,14 @@
 class LineItemsController < ApplicationController
+ 
+ def create
+  line_item = LineItem.create(:product_id => params[:product_id], :cart_id => current_cart.id, :order_id => current_order.id)
+  redirect_to user_cart_path(current_user, current_cart)
+ end
+ 
+ private
+ 
+ def line_item_params
+  params.require(:line_item).permit(:product_id, :cart_id, :quantity)
+ end
+ 
 end
