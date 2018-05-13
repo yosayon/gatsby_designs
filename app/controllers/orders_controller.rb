953f4a7ca749ec35b689bea0fc498105caf5b593
Yosa Yon <yosa.yon@gmail.com>
2018-05-13 08:37:00 +0000

    moved thank_you action from static to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+ 
+ def show
+  @order = Order.find(params[:id])
+ end
+ 
+ def thank_you
+  
+ end
+ 
+ def index
+ end
+ 
+ private
+ def order_params
+  params.require(:order).permit(:user_id)
+ end
 end
