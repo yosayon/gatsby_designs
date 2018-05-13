b128e4663f612615100838d13d65413107e084cb
Yosa Yon <yosa.yon@gmail.com>
2018-05-13 15:27:54 +0000

    updated total method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,15 @@ class Cart < ApplicationRecord
  has_many :line_items
  has_many :products, :through => :line_items
  belongs_to :user
+ 
+ def total
+  prices = []
+  self.line_items.each{|line_item| prices << line_item.product.price}
+  @total = (prices.reduce(:+))
+  if @total.nil?
+   @total = 0
+  else
+   @total.floor
+  end
+ end
 end
