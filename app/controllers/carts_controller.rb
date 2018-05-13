170771c842b0d9d4d0566ab7449dac9506568fd1
Yosa Yon <yosa.yon@gmail.com>
2018-05-13 08:38:30 +0000

    updated order_path to order_complete_path in update action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,10 +5,9 @@ class CartsController < ApplicationController
  
  def update
   @user = User.find(params[:user_id])
-  #checking out just means "paying" and starting with a new cart
   @user.check_out_order
   @user.current_order.check_out_order
-  redirect_to order_path
+  redirect_to order_complete_path
  end
  
  private
