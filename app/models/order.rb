e0bb2f3f5632909c544f940e719eaa7b010b5c68
Yosa Yon <yosa.yon@gmail.com>
2018-05-13 08:39:12 +0000

    added total instance method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,14 @@
 class Order < ApplicationRecord
  has_many :line_items
+ has_many :products, :through => :line_items
  
  def check_out_order
   self.update(:checkout => true)
+ end
+ 
+ def total
+  prices = []
+  self.line_items.each{|line_item| prices << line_item.product.price}
+  @total = prices.reduce(:+).floor
  end
 end
