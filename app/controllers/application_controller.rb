3c9353e1b6c2b50c5e210b4578dc4dce0c1a4b02
Yosa Yon <yosa.yon@gmail.com>
2018-05-13 15:24:32 +0000

    added current_order as helper method so we can call in in the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
  protect_from_forgery with: :exception
- helper_method :current_user, :logged_in?
+ helper_method :current_user, :logged_in?, :current_cart, :current_order
  
  def current_user
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
