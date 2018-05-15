51e50117f5a2a40e54850d2a000d9f17adaa75f8
Yosa Yon <yosa.yon@gmail.com>
2018-05-15 00:17:02 +0000

    added - and + and delete from cart options

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,14 @@ class LineItem < ApplicationRecord
  belongs_to :product
  belongs_to :cart
  belongs_to :order
+ 
+ def add_quantity
+  self.update(:quantity => self.quantity + 1)
+ end
+ 
+ def subtract_quantity
+  self.update(:quantity => self.quantity - 1)
+ end
+ 
+ 
 end
