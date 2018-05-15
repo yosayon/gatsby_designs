669c4c8daf252fa5b03e7bf0cdbf1fbd962bfecb
Yosa Yon <yosa.yon@gmail.com>
2018-05-15 02:46:51 +0000

    added destroy action and updated update action to remove line item from cart if quantity is equal to or less than 0

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -12,7 +12,19 @@ class LineItemsController < ApplicationController
  end
  
  def update
-  
+  @line_item = LineItem.find_by_id(params[:id])
+  @line_item.subtract_quantity
+  if @line_item.quantity <= 0
+   current_cart.line_items.delete(@line_item)
+  end
+  redirect_to user_cart_path(current_user, current_cart)
+ end
+ 
+ def destroy
+  product = Product.find_by_id(params[:product_id])
+  line_item = LineItem.find(params[:id])
+  product.line_items.destroy(line_item)
+  redirect_to user_cart_path(current_user, current_cart)
  end
  
  private
@@ -20,5 +32,4 @@ class LineItemsController < ApplicationController
  def line_item_params
   params.require(:line_item).permit(:product_id, :cart_id, :quantity)
  end
- 
 end
