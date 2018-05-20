21c2a870c27872faa45affb9b14f29751380b5d0
Yosa Yon <yosa.yon@gmail.com>
2018-05-20 08:28:14 +0000

    no changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,17 @@ class Order < ApplicationRecord
  
  def total
   prices = []
-  self.line_items.each{|line_item| prices << line_item.product.price}
-  @total = (prices.reduce(:+)).floor
+  self.line_items.each{|line_item| prices << line_item.product.price * line_item.quantity}
+  @total = (prices.reduce(:+))
+  if @total.nil?
+   @total = 0
+  else
+   @total.floor
+  end
  end
+ 
+ def self.checked_out_orders
+  all.where(:checkout => true)
+ end
+  
 end
