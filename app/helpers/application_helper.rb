2997956f3a177d4d583a10be0f1678c0a53025c6
Yosa Yon <yosa.yon@gmail.com>
2018-05-20 08:26:43 +0000

    updated show_name_if_signed_in to not show temp_user.name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,11 @@ module ApplicationHelper
  
  def show_name_if_signed_in
   if logged_in?
-   render :partial => 'layouts/nav_name'
+   if current_user.name == "temp_user"
+    render :partial => 'layouts/nav_sign_in'
+   else
+    render :partial => 'layouts/nav_name'
+   end
   else
    render :partial => 'layouts/nav_sign_in'
   end
