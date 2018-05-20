8c64b63ad54367ee06da923a0d7aa3543596e53e
Yosa Yon <yosa.yon@gmail.com>
2018-05-20 08:30:13 +0000

    added and then removed login_from_checkout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,37 @@
 Rails.application.routes.draw do
-  #resources :reviews
-  #resources :orders
-  #resources :users
+ 
+ #statics controller
    root 'statics#home'
    get '/about', :to => 'statics#about'
    get '/contact', :to => 'statics#contact'
+ 
+ #orders#thank_you
    get '/order_complete', :to => 'orders#thank_you'
    
-   
+ #sessions controller
    get '/login', :to => 'sessions#new'
    post '/login', :to => 'sessions#create'
-   get '/signup', :to => 'users#new'
-   post '/signup', :to => 'users#create'
    get '/auth/facebook/callback' => 'sessions#create'
    get 'logout' => 'sessions#destroy'
-   
-   resources :products
-   
+   #get '/login/from_checkout', :to => 'sessions#login_from_checkout'
+ 
+ #users controller  
+   get '/signup', :to => 'users#new'
+   post '/signup', :to => 'users#create'
+ 
+ #nested user and cart routes. A user should be able to view/update his/her cart only.  
    resources :users, :only => [:show] do 
     resources :carts, :only => [:show, :update]
    end
-
+ 
+ #nested user and orders route. A user can only view his/her order and orders. 
    resources :users, :only => [:show] do
     resources :orders, :only => [:show, :index]
    end
-   
+ 
+  resources :products, :only => [:index]
+ 
+ #nested product and line_items route. A user can create/update/destoy the line_items for the different products they have in their cart  
    resources :products, :only => [:show] do
     resources :line_items, :only => [:create, :update, :destroy]
    end
