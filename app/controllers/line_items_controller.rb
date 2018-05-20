6974955a712f5415caa19488e07746b4a5aa0e7c
Yosa Yon <yosa.yon@gmail.com>
2018-05-20 08:23:13 +0000

    updated current_user to session_user

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,14 +2,15 @@ class LineItemsController < ApplicationController
  
  def create
   @product = Product.find_by_id(params[:product_id])
-  if current_order.product_ids.include?(@product.id)
-   line_item = LineItem.find_by(:order_id => current_order.id, :product_id => @product.id)
-   line_item.add_quantity
-  else
-   line_item = LineItem.create(:product_id => @product.id, :cart_id => current_cart.id, :order_id => current_order.id, :quantity => 1)
+   if current_order.product_ids.include?(@product.id)
+    line_item = LineItem.find_by(:order_id => current_order.id, :product_id => @product.id)
+    line_item.add_quantity
+    redirect_to user_cart_path(session_user, current_cart)
+   else
+    line_item = LineItem.create(:product_id => @product.id, :cart_id => current_cart.id, :order_id => current_order.id, :quantity => 1)
+    redirect_to user_cart_path(session_user, current_cart)
+   end
   end
-  redirect_to user_cart_path(current_user, current_cart)
- end
  
  def update
   @line_item = LineItem.find_by_id(params[:id])
@@ -17,14 +18,14 @@ class LineItemsController < ApplicationController
   if @line_item.quantity == 0
    current_cart.line_items.destroy(@line_item)
   end
-  redirect_to user_cart_path(current_user, current_cart)
+  redirect_to user_cart_path(session_user, current_cart)
  end
  
  def destroy
   product = Product.find_by_id(params[:product_id])
   line_item = LineItem.find(params[:id])
   product.line_items.destroy(line_item)
-  redirect_to user_cart_path(current_user, current_cart)
+  redirect_to user_cart_path(session_user, current_cart)
  end
  
  private
@@ -32,4 +33,5 @@ class LineItemsController < ApplicationController
  def line_item_params
   params.require(:line_item).permit(:product_id, :cart_id, :quantity)
  end
+ 
 end
