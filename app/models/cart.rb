967dbd06953acb5290861e9d79b6681e96116a18
Yosa Yon <yosa.yon@gmail.com>
2018-05-21 06:03:05 +0000

    added merge_cart_with(temp_user) method to merge cart items with existing user account

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -14,5 +14,21 @@ class Cart < ApplicationRecord
   end
  end
  
- 
+ def merge_cart_items_with(temp_user)
+  if self.line_items.empty?
+   temp_user.cart.line_items.each{|line_item| line_item.update(:order_id => self.user.current_order.id, :cart_id => self.id)}
+  else
+   temp_user.cart.product_ids.each do |id|
+    if self.product_ids.include?(id)
+    line_item = LineItem.find_by(:cart_id => self.id, :product_id => id, :order_id => self.user.current_order)
+    line_item.add_quantity
+    else
+    line_item = LineItem.find_by(:cart_id => temp_user.cart.id, :product_id => id, :order_id => temp_user.current_order.id)
+    line_item.update(:order_id => self.user.current_order.id, :cart_id => self.id)
+    self.line_items << line_item
+    end
+   end
+  end
+ end
+
 end
