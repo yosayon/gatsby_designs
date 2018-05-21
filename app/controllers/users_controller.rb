c0dc11f46bb98df463dd5b6ca9323084dda93ab3
Yosa Yon <yosa.yon@gmail.com>
2018-05-21 16:50:06 +0000

    added conditional callback to only purge temp user data on create if the user successfully logs in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+ after_action :purge_temp_user_data, :only => [:create], :if => :logged_in?
  
  def new
   @user = User.new
@@ -8,6 +9,7 @@ class UsersController < ApplicationController
   @user = User.new(user_params)
   if @user.save
    session[:user_id] = @user.id
+   merge_cart_items if session[:temp_id]
    redirect_to root_path
   else
    render :new
