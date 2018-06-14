0946b5e12fa4ebb400f97ddd07f0a9adfb7f1978
Yosa Yon <yosa.yon@gmail.com>
2018-06-14 02:32:13 +0000

    refactored entire code to have javascript model objects

--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -1,52 +1,59 @@
-let getOrders = function(orders){
- let id = $(".current_user")[0].id
- var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  $.get(`/users/${id}/orders`, function(orders){
-   orders.data.map(function(order){
-    //console.log(order.attributes["created-at"]);
-   var order_date  = new Date(order.attributes["created-at"]);
-    $('#user-orders').append(`<li><a href="/users/${id}/orders/${order.id}">
-    Order Number: ${order.id} ${order_date.toLocaleDateString("en-US")}</a>
-    </li>`);
+const bindOrderHandlers = () => {
+ let id = $(".current_user")[0].id;
+ $("#button-orders").click((e) => {
+  $.get(`/users/${id}/orders.json`, (response) => {
+   $('#user-orders').html('');
+   response.data.forEach(order => {
+    let newOrder = new Order(order)
+    let orderHTML = newOrder.formatIndex();
+    $('#user-orders').append(orderHTML);
    })
+  })
  })
 }
 
-let getReviews = function(reviews){
- let id = $(".current_user")[0].id
- $.get(`/users/${id}/reviews`, function(reviews){
-  console.log(reviews);
-   reviews.data.forEach(function(review){
-    let newReview = new Review(review)
-    console.log(newReview);
-    let reviewHTML = `
-    <li><a href="/users/${id}/reviews/${newReview.id}">${newReview.title}</a></li>
-    `
-    $("#user-reviews").append(reviewHTML)
-   })
- })
+function Order(order){
+ this.id = order.id,
+ this.user_id = order.attributes["user-id"],
+ this.created_at = order.attributes["created-at"]
 }
 
-let attachListeners = function(){
- $("#button-orders").click(function(){
-  //console.log("I just hijacked this button!");
-  if ($("#user-orders").text() === ""){
-   getOrders(this)
-  }
- })
- $("#button-reviews").click(function(){
-  if ($("#user-orders").text() === ""){
-   getReviews(this)
-  }
- })
+Order.prototype.formatTime = function(){
+ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+ let order_date  = new Date(this.created_at);
+ return order_date.toLocaleDateString("en-US");
 }
 
-function Review(review){
- this.id = review.id,
- this.title = review.attributes.title,
- this.comment = review.attributes.comment,
- this.user_id = review.attributes.user_id,
- this.product_id = review.attributes.product_id
+Order.prototype.formatIndex = function(){
+ let orderHTML = `
+  <li>
+   <a href="/users/${this.user_id}/orders/${this.id}"> Order Number: ${this.id} ${this.formatTime()}</a>
+  </li>
+ `
+ return orderHTML;
 }
 
-$(".users.show").ready(attachListeners);
+$(".users.show").ready(bindOrderHandlers)
+
+// function Review(review){
+//  this.id = review.id,
+//  this.title = review.attributes.title,
+//  this.comment = review.attributes.comment,
+//  this.user_id = review.attributes.user_id,
+//  this.product_id = review.attributes.product_id
+// }
+
+// let getReviews = function(reviews){
+//  let id = $(".current_user")[0].id
+//  $.get(`/users/${id}/reviews`, function(reviews){
+//   console.log(reviews);
+//    reviews.data.forEach(function(review){
+//     let newReview = new Review(review)
+//     //console.log(newReview);
+//     let reviewHTML = `
+//     <li><a href="/users/${id}/reviews/${newReview.id}">${newReview.title}</a></li>
+//     `
+//     $("#user-reviews").append(reviewHTML)
+//    })
+//  })
+// }
