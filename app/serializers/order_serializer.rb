e0e7fea3219d80810fb08c7529a506feb36dabf0
Yosa Yon <yosa.yon@gmail.com>
2018-06-17 06:52:15 +0000

    removed association macros and added line_items and products as attributes

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :created_at
-  has_many :line_items
-  has_many :products, :through => :line_items
+  attributes :id, :user_id, :created_at, :line_items, :products
+
 end
