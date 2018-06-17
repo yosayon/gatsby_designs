efbac3aa3492ef78ed40bdcac9cb7589f28a6e1f
Yosa Yon <yosa.yon@gmail.com>
2018-06-17 07:31:36 +0000

    added respond_to to product show controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,11 +6,15 @@ class ProductsController < ApplicationController
  
  def show
   @product = Product.find_by_id(params[:id])
+  respond_to do |f|
+   f.html
+   f.json {render :json => @product}
+  end
  end
  
  def highest_rated
   @products = Product.by_highest_rating
   render :index
  end
- 
+
 end
