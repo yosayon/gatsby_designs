0b98f43ef7b8cca5c40e249f867214fbc7b72d5f
Yosa Yon <yosa.yon@gmail.com>
2018-06-18 02:07:04 +0000

    separated function model from users_show.js for readability. Fixed Order function to assign correct attributes and fixed Order prototype methods. User can now see order show page by hovering over each order

--- app/assets/javascripts/model.js
+++ app/assets/javascripts/model.js
@@ -1,14 +1,69 @@
-const methodConstructor = function(){
- return class{
-  constructor(x){
-   this.id = x.id
+const Order = function(order){
+ this.id = order.id,
+ this.user_id = order.user_id,
+ this.created_at = order.created_at,
+ this.line_items = order.line_items,
+ this.products = order.products
+}
+
+Order.prototype.orderShowTemplate = function(){
+ let template = Handlebars.compile($("#order-show-template")[0].innerHTML)
+ return template;
+}
+
+Order.prototype.total = function(){
+ let prices = [];
+ let total = 0;
+  for(let i = 0; i < this.products.length; i++){
+   prices.push(Math.floor(this.products[i].price * this.line_items[i].quantity))
   }
-  static all(){
-   let collection = [];
-   $.get(`/users/`)
-  }
+  return total = prices.reduce((sum,val) => sum + val);
+}
+
+Order.prototype.formatTime = function(){
+ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+ let order_date  = new Date(this.created_at);
+ return order_date.toLocaleDateString("en-US");
+}
+
+Order.prototype.formatIndex = function(){
+ let orderHTML = `
+  <li>
+   <a href="/users/${this.user_id}/orders/${this.id}"> Order Number: ${this.id} ${this.formatTime()}</a>
+  </li>
+ `
+ return orderHTML;
+}
+
+Order.prototype.insertIntoPartial = function(){
+ let order_id = this.id;
+ let products = this.products;
+ let total = this.total();
+ let date = this.formatTime();
+ line_items = [];
+ for(let i = 0; i < products.length; i++){
+   let obj = {
+    product_picture_path: `/products/${products[i].id}`,
+    product_picture: `${products[i].picture}`,
+    product_name: products[i].name,
+    product_price: Math.floor(products[i].price),
+    quantity: this.line_items[i].quantity
+   }
+  line_items.push(obj);
  }
-};
+ return {order_id, line_items, total, date}
+}
+
+const Review = function(review){
+  this.id = review.id,
+  this.title = review.title,
+  this.comment = review.comment
+ }
+ 
+Review.prototype.reviewShowTemplate = function(){
+ let template = Handlebars.compile($("#review-show-template")[0].innerHTML)
+ return template;
+}
 
 
  
--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -9,9 +9,8 @@ const bindOrderHandlers = () => {
  $("#button-orders").click((e) => {
   $.get(`/users/${id}/orders.json`, (response) => {
    $('#user-orders').html('');
-   response.data.forEach(order => {
+   response.orders.forEach(order => {
     let newOrder = new Order(order)
-    console.log(newOrder);
     let orderHTML = newOrder.formatIndex();
     $('#user-orders').append(orderHTML);
    })
@@ -24,67 +23,12 @@ const bindOrderShowHandlers = () => {
  $('#user-orders li a').hover(function(e){
  e.preventDefault();
  $.get(`${this.href}.json`, (response) => {
-  let newOrder = new Order(response.data)
-  console.log(response);
+  let newOrder = new Order(response.order)
   let template = newOrder.orderShowTemplate();
   let results = newOrder.insertIntoPartial();
   $("#user-orders-show")[0].innerHTML = template(results)
   })
  })
-}
-
-function Order(order){
- this.id = order.id,
- this.user_id = order.attributes["user-id"],
- this.created_at = order.attributes["created-at"]
-}
-
-Order.prototype.insertIntoPartial = function(){
- let order_id = this.id;
- let products = this.products;
- let total = this.total();
- let date = this.formatTime();
- line_items = [];
- for(let i = 0; i < products.length; i++){
-   let obj = {
-    product_picture_path: `/products/${products[i].id}`,
-    product_picture: `${products[i].picture}`,
-    product_name: products[i].name,
-    product_price: Math.floor(products[i].price),
-    quantity: this.line_items[i].quantity
-   }
-  line_items.push(obj);
- }
- return {order_id, line_items, total, date}
-}
-
-Order.prototype.formatTime = function(){
- const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
- let order_date  = new Date(this.created_at);
- return order_date.toLocaleDateString("en-US");
-}
-
-Order.prototype.formatIndex = function(){
- let orderHTML = `
-  <li>
-   <a href="/users/${this.user_id}/orders/${this.id}"> Order Number: ${this.id} ${this.formatTime()}</a>
-  </li>
- `
- return orderHTML;
-}
-
-Order.prototype.orderShowTemplate = function(){
- let template = Handlebars.compile($("#order-show-template")[0].innerHTML)
- return template;
-}
-
-Order.prototype.total = function(){
- let prices = [];
- let total = 0;
-  for(let i = 0; i < this.products.length; i++){
-   prices.push(Math.floor(this.products[i].price * this.line_items[i].quantity))
-  }
-  return total = prices.reduce((sum,val) => sum + val);
 }
 
 const bindReviewHandlers = () =>{
@@ -121,29 +65,6 @@ const bindReviewShowHandlers = () =>{
  })
 }
 
- function Review(review){
-  this.id = review.id,
-  this.title = review.attributes.title,
-  this.comment = review.attributes.comment
- }
- 
-Review.prototype.reviewShowTemplate = function(){
- let template = Handlebars.compile($("#review-show-template")[0].innerHTML)
- return template;
-}
-
-function Product(product){
- this.id = product.id,
- this.name = product.attributes.name,
- this.picture = product.attributes.picture,
- this.price = product.attributes.price,
- this.average_rating = Math.floor(product.attributes['average-rating'])
-}
-
-const methods = function(instance){
- instance.class
-}
- 
  function handlebarsSetup(){
   Handlebars.registerPartial('lineItemPartial', $("#line-item-partial")[0].innerHTML);
  }
