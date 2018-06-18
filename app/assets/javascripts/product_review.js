e28922d9f96d9f27b54a869494cb55a77071899d
Yosa Yon <yosa.yon@gmail.com>
2018-06-18 02:52:57 +0000

    edited product show page to include button for rendering a new review form and added product_review.js for page specific js

---
+++ app/assets/javascripts/product_review.js
@@ -0,0 +1,9 @@
+const attachReviewListener = () => {
+ $("#button-reviews").click(function(e){
+  e.preventDefault();
+  debugger;
+  //$.get()
+ })
+}
+
+$(".products.show").ready(attachReviewListener)
