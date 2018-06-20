fc763581b212e9c2082458b1eef37ed3406cfda4
Yosa Yon <yosa.yon@gmail.com>
2018-06-20 22:24:42 +0000

    updated toggleReviewForm to getReviewForm, pass form into Review.submitReview, added .success and .fail, added handlers Review.success and Review.fail, when fail, show validation errors

--- app/assets/javascripts/product_review.js
+++ app/assets/javascripts/product_review.js
@@ -1,68 +1,66 @@
-Review.toggleReviewForm = () => {
+Review.getReviewForm = () => {
  $("#button-reviews a").click(function(e){
   e.preventDefault();
+  let url = this.href
   $.get(`${this.href}`, (response) => {
    let form = response;
    $("#review-form-container")[0].innerHTML = form
-   Review.submitNewReview();
-   Review.displayFormErrors();
+   Review.submitReview(form)
   })
  })
 }
 
-Review.submitNewReview = function(){
+Review.submitReview = function(form){
  $("#new_review").submit(function(e){
   e.preventDefault();
-  let $form = $(this);
-  let action = `${$form.attr("action")}.json`;
-  let params = $form.serialize();
-  $.post(action, params)
-  .always(Review.displayFormErrors)
- })
+   let $form = $(this);
+   let action = `${$form.attr("action")}.json`;
+   let params = $form.serialize();
+   let posting = $.post(action, params)
+   .success(Review.toggleReviewForm(form),Review.success)
+   .fail(Review.toggleReviewForm(form), Review.fail)
+  })
 }
 
-Review.success = function(data){
-  $("#review-form-container")[0].innerHTML = ''
-  $("div.field")[0].className === 'field '
-  $("#title-error, #comment-error, #rating-error").innerHTML = '';
-  $(':input[type="submit"]').prop('disabled', false);
-  let newReview = new Review(data)
-  let review = review.renderProductReviewTemplate();
-  $("#product-review-container").append(review)
-   Review.displayFormErrors(json)
+Review.toggleReviewForm = function(form){
+ $("#review-form-container")[0].innerHTML = form
+ Review.submitReview(form)
 }
 
-
-Review.displayFormErrors = function(json){
- let title = $("input#review_title").val()
- let comment = $("input#review_comment").val()
- let radio = $(":input[type='radio']").filter(":checked");
-
- if(title && comment && radio){
-  $("#new_review").submit(function(e){
-  e.preventDefault();
-  let $form = $(this);
-  let action = `${$form.attr("action")}.json`;
-  let params = $form.serialize();
-   $.post(action, params)
-   .success(Review.success)
-   })
- }else{
-   let errors = json[1]
-   if ($("div.field")[0].className === 'field '){
-   $("div.field").toggleClass(' field_with_errors')
+ Review.fail = function(xhr){
+  let errors = xhr.responseJSON.error
+  let fields = xhr.responseJSON.text
+  
+  for(let attribute in errors){
+   if(attribute === "title"){
+    $(".field ").eq(0).removeClass('field ').addClass('field field_with_errors')
+    $("#title-error")[0].innerHTML = errors[attribute][0]
+   }else{
+    $("#review_title").val(fields.title)
    }
-   errors.title ? $("#title-error")[0].innerHTML = errors.title[0] : $("#title-error")[0].innerHTML = ''
-   errors.comment ? $("#comment-error")[0].innerHTML = errors.comment[0] : $("#comment-error")[0].innerHTML = ''
-   errors.product_rating ? $("#rating-error")[0].innerHTML = errors.product_rating[0] : $("#rating-error")[0].innerHTML = ''
-   $(':input[type="submit"]').prop('disabled', false);
-   Review.submitNewReview();
+   if(attribute === "comment"){
+    $(".field ").eq(1).removeClass('field ').addClass('field field_with_errors')
+    $("#comment-error")[0].innerHTML = errors[attribute][0]
+   }else{
+    $("#review_comment").val(fields.comment)
+   }
+   if(attribute === "product_rating"){
+    $("#rating-error")[0].innerHTML = errors[attribute][0]
+   }else{
+     $(`input#review_product_rating_${fields.product_rating}`).prop('checked', true)
+   }
+  }
  }
-}
 
+Review.success = function(response){
+ console.log("It was a success!!!")
+  let newReview = new Review(response.review)
+  let review = newReview.renderProductReviewTemplate();
+  $("#product-review-container").append(review)
+}
 
 $(".products.show").ready(() =>{
- Review.templateProductSource = $("#product-review-template").html();
+ Review.templateProductSource = $("#product-review-template").html() || '';
  Review.templateForProductReview = Handlebars.compile(Review.templateProductSource);
- Review.toggleReviewForm();
+ Review.getReviewForm();
 })
