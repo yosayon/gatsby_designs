35988587b00731cba08f4b9c69a7909df508e6f2
Yosa Yon <yosa.yon@gmail.com>
2018-06-21 21:03:55 +0000

    added Product.goBack and Product.goNext, history.replaceState, user can now go back and next to see the product show page

--- app/assets/javascripts/product_show.js
+++ app/assets/javascripts/product_show.js
@@ -1,23 +1,34 @@
 
 Product.attachBackNextHandlers = () => {
- console.log("hitting home")
  $("#back").click(Product.goBack)
  $("#next").click(Product.goNext)
 }
 
 Product.goBack = function(){
- console.log("going back")
  let id = parseInt($("#back").attr("data-id"));
  id === 1 ? id = 1 : id--
  $.get("/products/" + id + ".json", function(data){
   let newProduct = new Product(data.product)
   let product = newProduct.renderProductShowTemplate();
-  $(".products-container").html(product)
+  $("#products-show").html(product)
+  history.replaceState(null, null, id);
+  Product.attachBackNextHandlers();
  })
- 
 }
 
 Product.goNext = function(){
- console.log("I'm going next")
+ console.log("I'm going next");
+ let id = parseInt($("#next").attr("data-id"));
+ id === 15 ? id = 15 : id++;
+  $.get("/products/" + id + ".json", function(data){
+  let newProduct = new Product(data.product)
+  let product = newProduct.renderProductShowTemplate();
+  $("#products-show").html(product)
+  history.replaceState(null, null, id);
+  Product.attachBackNextHandlers();
+ })
 }
 
+$("#backnext").ready(() => {
+ Product.attachBackNextHandlers();
+})
