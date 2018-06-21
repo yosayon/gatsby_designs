99f8bbbd3fec5274ae26d580d4c67a12b11fa791
Yosa Yon <yosa.yon@gmail.com>
2018-06-21 03:17:45 +0000

    added belongs_to :user

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :comment, :user_id, :product_id, :product_rating, :created_at
   belongs_to :product
+  belongs_to :user
 end
