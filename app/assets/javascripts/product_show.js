1e7483b5ab03d24f19ce0275562330227674e153
Yosa Yon <yosa.yon@gmail.com>
2018-06-22 04:55:57 +0000

    added more functionaliy to goNext and goBack Product functions. reordered the way the content appends to product show page, added history.replaceState to update url

--- app/assets/javascripts/product_show.js
+++ app/assets/javascripts/product_show.js
@@ -1,31 +1,47 @@
-
 Product.attachBackNextHandlers = () => {
  $("#back").click(Product.goBack)
  $("#next").click(Product.goNext)
 }
 
 Product.goBack = function(){
+ console.log("went back for no reason!")
+ Review.getReviewForm();
  let id = parseInt($("#back").attr("data-id"));
  id === 1 ? id = 1 : id--
  $.get("/products/" + id + ".json", function(data){
   let newProduct = new Product(data.product)
   let product = newProduct.renderProductShowTemplate();
-  $("#products-show").html(product)
+  $(".products.show").html(product)
+  $("#product-review-container").html('')
+  newProduct.reviews.forEach(review => {
+    let newReview = new Review(review)
+    let reviewContent = newReview.renderProductReviewTemplate()
+    $("#product-review-container").append(reviewContent)
+  })
   history.replaceState(null, null, id);
   Product.attachBackNextHandlers();
+  Review.getReviewForm();
+  Product.addToCart()
  })
 }
 
 Product.goNext = function(){
- console.log("I'm going next");
  let id = parseInt($("#next").attr("data-id"));
  id === 15 ? id = 15 : id++;
-  $.get("/products/" + id + ".json", function(data){
+ $.get("/products/" + id + ".json", function(data){
   let newProduct = new Product(data.product)
   let product = newProduct.renderProductShowTemplate();
-  $("#products-show").html(product)
+  $(".products.show").html(product)
+  $("#product-review-container").html('')
+  newProduct.reviews.forEach(review => {
+    let newReview = new Review(review)
+    let reviewContent = newReview.renderProductReviewTemplate()
+    $("#product-review-container").append(reviewContent)
+  })
   history.replaceState(null, null, id);
+  Review.getReviewForm();
   Product.attachBackNextHandlers();
+  Product.addToCart();
  })
 }
 
